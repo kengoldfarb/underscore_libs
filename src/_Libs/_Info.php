d41132b9c86d63c4dd4285cd8cd85c77700022dc
Ken <ken@kengoldfarb.com>
2013-08-20 14:04:50 -0600

    add info class

---
+++ src/_Libs/_Info.php
@@ -0,0 +1,16 @@
+<?php
+class _Info{
+    public static function _getUserIpAddr ()
+    {
+        $ip = 'unknown.ip.address';
+        if(isset($_SERVER)){
+	        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) // If server is behind a load balancer
+			{
+	            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	        } elseif (!empty($_SERVER['REMOTE_ADDR'])) {
+	            $ip = $_SERVER['REMOTE_ADDR'];
+	        }
+	    }
+        return $ip;
+    }
+}
